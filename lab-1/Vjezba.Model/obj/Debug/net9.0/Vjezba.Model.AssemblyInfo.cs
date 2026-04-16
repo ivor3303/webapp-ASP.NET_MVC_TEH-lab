139dc31b34f2d10a3dd5e4ad57abf4f89ec32cae
Ivor Županić <izupanic@tvz.hr>
2026-04-16 17:02:18 +0200

    Dovrsen Lab 2 MVC dio projekta

--- lab-1/Vjezba.Model/obj/Debug/net9.0/Vjezba.Model.AssemblyInfo.cs
+++ lab-1/Vjezba.Model/obj/Debug/net9.0/Vjezba.Model.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vjezba.Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e79bc62cff26d60b985fbab61419a84b80ced7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+386464a385593424b88f2caafa53eb525af4f1ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vjezba.Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vjezba.Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
