7baab9ecbb1358b93e2a053f03e9ada34dd1957a
Ivor Županić <izupanic@tvz.hr>
2026-07-09 13:44:15 +0200

    FINAL

--- lab-1/Vjezba.Model/obj/Debug/net9.0/Vjezba.Model.AssemblyInfo.cs
+++ lab-1/Vjezba.Model/obj/Debug/net9.0/Vjezba.Model.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vjezba.Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d58a82dcb251ab9fdf4445258125cefa5c517c09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f9c71762d71eef3ff615e796db7c288c084e8b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vjezba.Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vjezba.Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
